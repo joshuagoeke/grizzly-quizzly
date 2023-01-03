f3f4f03b9316a1ea3323715fe4f7191b62cc94d3
joshuagoeke <113941639+joshuagoeke@users.noreply.github.com>
2023-01-03 14:58:56 -0600

    adding Leah's super helpful pseudo-code.

---
+++ assets/script.js
@@ -0,0 +1,19 @@
+// User is presented with 5 questions - array of objects
+    //  Format for questions???
+        // Object that conatins {question: string, Choices: array, answer: string}
+        // Display question - loop
+        //  append question
+        // append choices array
+
+// user selects an answer(button,radio,checkboxes)data-answer = ""
+    //  click event is on the parent container
+    //  how to know which element was clicked(event.target)
+    // if the answer is correct display next quesrion access array of questions object 
+    // if answer is incorrect add 15 seconds to score and display next question
+
+    //  When all questions are answered display form to submit initials
+    //  save form values score and initials to local STrorage
+
+//  change to highscores HTML
+    //  read values from localstorage
+    //  append score values to page

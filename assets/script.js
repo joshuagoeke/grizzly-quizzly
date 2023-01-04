e2f82fe37db5a6808b21d2dbfdf992a0ed6b1502
joshuagoeke <113941639+joshuagoeke@users.noreply.github.com>
2023-01-03 20:34:05 -0600

    Made some questions, added .css styling

--- assets/script.js
+++ assets/script.js
@@ -17,3 +17,23 @@
 //  change to highscores HTML
     //  read values from localstorage
     //  append score values to page
+
+var theWholeShebang = [
+    question1 = {
+        question: "What did Guido von Rossum name his programming language, Python, after?",
+        answer1: "Slytherin House",
+        answer2: "The snake-like nature of the code",
+        answer3: "Monty Python's Flying Circus",
+        answer4: "Very long string objects",
+        DingDing: 3,
+    },
+
+    question2 = {
+        question: "The award-winning Monty Python CD-ROM from 1994 was called:",
+        answer1: "Life of Brian",
+        answer2: "Complete Waste of Time",
+        answer3: "Quest for the Holy Grail",
+        answer4: "A Fish Called Wanda",
+        DingDing: 2,
+    },
+]

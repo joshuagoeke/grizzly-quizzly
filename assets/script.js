299c0391895a522c7ca842ebcf9c4206c47474b9
joshuagoeke <113941639+joshuagoeke@users.noreply.github.com>
2023-01-04 23:39:04 -0600

    got questions appearing using Myeonghun Jeong's method

--- assets/script.js
+++ assets/script.js
@@ -18,22 +18,61 @@
     //  read values from localstorage
     //  append score values to page
 
+//Variables
+    var liveQuestion = document.querySelector("#question");
+    // var liveAnswerStem = document.querySelector('.abcd-container');
+    
+    var livePrefixA = document.querySelector('#A');
+    var livePrefixB = document.querySelector('#B');
+    var livePrefixC = document.querySelector('#C');
+    var livePrefixD = document.querySelector('#D');
+
+    var liveOption0 = document.querySelector('#option0');
+    var liveOption1 = document.querySelector('#option1');
+    var liveOption2 = document.querySelector('#option2');
+    var liveOption3 = document.querySelector('#option3');
+
 var theWholeShebang = [
-    question1 = {
+    {
         question: "What did Guido von Rossum name his programming language, Python, after?",
         answer1: "Slytherin House",
         answer2: "The snake-like nature of the code",
         answer3: "Monty Python's Flying Circus",
         answer4: "Very long string objects",
-        DingDing: 3,
+        DingDing: 2,
     },
-
-    question2 = {
+    {
         question: "The award-winning Monty Python CD-ROM from 1994 was called:",
         answer1: "Life of Brian",
         answer2: "Complete Waste of Time",
         answer3: "Quest for the Holy Grail",
         answer4: "A Fish Called Wanda",
-        DingDing: 2,
+        DingDing: 1,
     },
-]
+    {
+        question: "What is the air-speed velocity of an unladen swallow?",
+        answer1: "42",
+        answer2: "Complete Waste of Time",
+        answer3: "9.8 m/s^2",
+        answer4: "An African or a European Swallow?",
+        DingDing: 4,
+    },
+    {
+        question: "Which Actor was in Harry Potter films and a Fish Called Wanda?",
+        answer1: "John Cleese",
+        answer2: "Terry Jones",
+        answer3: "Eric Idle",
+        answer4: "Michael Palin",
+        DingDing: 1,
+    },
+];
+
+liveQuestion.textContent = theWholeShebang[0].question; 
+liveOption0.textContent = theWholeShebang[0].answer1;
+liveOption1.textContent = theWholeShebang[0].answer2;
+liveOption2.textContent = theWholeShebang[0].answer3;
+liveOption3.textContent = theWholeShebang[0].answer4;
+
+
+console.log((theWholeShebang[1].question));
+
